b810d628e5af4606280b8b0532ff22c505f71a8b
Lauxx <lm.nichols@hotmail.com>
2016-03-04 13:57:23 -0700

    took notes on review of challenge was able to gt bonus

--- public/app.js
+++ public/app.js
@@ -8,7 +8,9 @@
 */}
 
 var PeopleApp = React.createClass({
-  
+  propTypes:{
+    url: React.PropTypes.string.isRequired
+  },
   getInitialState: function() {
     return {
       people: []
@@ -23,7 +25,7 @@ var PeopleApp = React.createClass({
       self.setState({people: data})
     })
   },
-  componentDidMount: function() {
+  componentDidMount: function() {//when mounting to page execute this data
     this.loadPeopleFromServer();
   },
   render: function() {
@@ -38,6 +40,7 @@ var PeopleApp = React.createClass({
 var PeopleList = React.createClass({
   
   render: function() {
+
     var person = this.props.peopleArray.map(function(p){
       return <Person username={p.username} img={p.img} country={p.country} 
       birth_date={p.birth_date}/>
@@ -57,7 +60,9 @@ var PeopleList = React.createClass({
   and calculate their age. Use this function to render the persons age.
 */}
 var Person = React.createClass({
-  
+  getAge: function(age){
+    return new Date().getYear() - new Date(age).getYear()
+  },
   render: function() {
     return (
       <div className="row">
@@ -66,7 +71,7 @@ var Person = React.createClass({
           <img src={this.props.img} className="img-thumbnail" />
             <h2>{this.props.username}</h2>
             <h3>{this.props.country}</h3>
-            <h4>{this.props.birth_date}</h4>
+            <h4>{this.getAge(this.props.birth_date)}</h4>
             
           </div>
         </div>
@@ -78,7 +83,7 @@ var Person = React.createClass({
 
 
 
-React.render(<PeopleApp url="/api/people/" />, 
+React.render(<PeopleApp url="/api/people" />, 
   document.getElementById('react-container'));
 
 
